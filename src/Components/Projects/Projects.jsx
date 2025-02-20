680618e98f667eba15b91454690b5e58f8397da0
Nilanjan Mondal <nilanjanmondal165@gmail.com>
2025-02-20 11:35:47 +0530

    added more responsiveness

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -4,42 +4,43 @@ import redBus from "../../assets/redBus.png";
 import fakeStore from "../../assets/fakeStore.png";
 import backEnd from "../../assets/backEnd.jpg";
 import pFolio from "../../assets/pFolio.png";
+
 const Projects = () => {
   return (
-    <div id="Projects" className=" p-10 md:p-20 text-white md:pt-0">
-      <h1 className="text-2xl  md:text-4xl text-white font-bold text-center md:pb-5 pb-3">Projects</h1>
-      <div className="py-12 px-[85px] md:px-[120px] flex flex-wrap gap-5">
+    <div id="Projects" className="p-10 md:p-20 text-white md:pt-0">
+      <h1 className="text-2xl md:text-4xl text-white font-bold text-center md:pb-5 pb-3">Projects</h1>
+      <div className="py-12 px-[20px] md:px-[120px] grid grid-cols-1 md:grid-cols-3 gap-5">
         <ProjectCard
           title="Redbus Clone"
           main="this is a redbus clone created using pure html and css"
-          codeUrl= "https://github.com/Nilanjan-Mondal/Redbus-Clone.git"
-          demoUrl={"https://redbus-clone-otb4p14m9-nilanjan-mondals-projects.vercel.app/"}
-          image={ redBus } 
+          codeUrl="https://github.com/Nilanjan-Mondal/Redbus-Clone.git"
+          demoUrl="https://redbus-clone-otb4p14m9-nilanjan-mondals-projects.vercel.app/"
+          image={redBus}
         />
         <ProjectCard
-          title="FakeStore UI" 
+          title="FakeStore UI"
           main="this is an ecommerce UI created using react js and tailwind css"
-          codeUrl={"https://github.com/Nilanjan-Mondal/FakeStore-UI"}
-          demoUrl={"https://fakestore-ui.vercel.app/"}
-          image={ fakeStore }
+          codeUrl="https://github.com/Nilanjan-Mondal/FakeStore-UI"
+          demoUrl="https://fakestore-ui.vercel.app/"
+          image={fakeStore}
         />
         <ProjectCard
-          title="PortFolio Website" 
+          title="PortFolio Website"
           main="this is my portfolio website created using react js and tailwind css"
-          image={ pFolio }
-          codeUrl={"https://github.com/Nilanjan-Mondal/PortFolio-Website"}
+          image={pFolio}
+          codeUrl="https://github.com/Nilanjan-Mondal/PortFolio-Website"
         />
         <ProjectCard
-          title="Restuarant Backend" 
-          main="This REST API powers a restaurant website using Node.js, Express.js, and MongoDB for data storage, with Cloudinary handling image uploads."  
-          codeUrl={"https://github.com/Nilanjan-Mondal/PizzaApp-Backend"}
-          image= { backEnd }
+          title="Restuarant Backend"
+          main="This REST API powers a restaurant website using Node.js, Express.js, and MongoDB for data storage, with Cloudinary handling image uploads."
+          codeUrl="https://github.com/Nilanjan-Mondal/PizzaApp-Backend"
+          image={backEnd}
         />
         <ProjectCard
-          title="Tweets Backend" 
-          main="This REST API powers a Tweeter website using Node.js, Express.js, and MongoDB for data storage, with Cloudinary handling image uploads."  
-          codeUrl={"https://github.com/Nilanjan-Mondal/TwitterApp"}
-          image= { backEnd }
+          title="Tweets Backend"
+          main="This REST API powers a Tweeter website using Node.js, Express.js, and MongoDB for data storage, with Cloudinary handling image uploads."
+          codeUrl="https://github.com/Nilanjan-Mondal/TwitterApp"
+          image={backEnd}
         />
       </div>
     </div>
